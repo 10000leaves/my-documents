732defdc49d89d08595e9b64dc6ad3d9823ca747
10000leaves <leaves.h7@gmail.com>
2024-04-24 03:43:59 +0000

    feat: 検索機能を追加

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,11 @@ const config = {
     locales: ['jp'],
   },
 
+  // 検索プラグイン
+  // https://github.com/cmfcmf/docusaurus-search-local/issues/199
+  // バグがありpackage.jsonを修正しているため注意
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
+
   presets: [
     [
       'classic',
